0cc346438b3ffabc1b65e98d1f5f72e78c05124b
Eliott Frohly <frohlyeliott@gmail.Com>
2024-02-06 15:15:27 -0500

    Final Commit

--- MarkerPose.py
+++ MarkerPose.py
@@ -1,16 +1,14 @@
 import cv2
 import pickle
 import cv2.aruco as aruco
+import numpy as np
 
-
-markerLength = 0.05
-# Load camera parameters from .pkl file
 with open('camera_params.pkl', 'rb') as f:
     camera_params = pickle.load(f)
 
-# Extract camera matrix and distCoeffs
 cameraMatrix = camera_params[0]
 distCoeffs = camera_params[1]
+markerLength = 0.05
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters = aruco.DetectorParameters_create()
@@ -19,8 +17,8 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    if not ret:
-        break
+    while(not ret):
+        ret,frame=cap.read()
 
     corners, ids, rejected = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
 
@@ -34,10 +32,22 @@ while True:
             print(f"Translation: {tvec}")
             print(f"Rotation: {rvec}")
 
-    cv2.imshow("Frame", frame)
+            rotation_matrix = cv2.Rodrigues(rvec)[0]
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            normal = rotation_matrix[:, 2] 
+
+            a, b, c = normal
+            d = -np.dot(normal, tvec)
+
+            print(f"Équation du plan : {a}x + {b}y + {c}z + {d} = 0")
+
+            homogeneous_transform = np.column_stack((rotation_matrix, tvec))
+            homogeneous_transform = np.vstack([homogeneous_transform, [0, 0, 0, 1]])
+
+        cv2.imshow("Frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
--- getCalibrationPics.py
+++ getCalibrationPics.py
@@ -4,47 +4,46 @@ import time
 import os
 import shutil
 
-def recordCalibrationPics():
-    filesInitial = os.listdir("CalibrationPics/Initial")
-    filesFinal = os.listdir("CalibrationPics/Final")
+filesInitial = os.listdir("CalibrationPics/Initial")
+filesFinal = os.listdir("CalibrationPics/Final")
 
-    for frame in filesInitial:
-        os.remove("CalibrationPics/Initial/"+frame)
-    for frame in filesFinal:
-        os.remove("CalibrationPics/Final/"+frame)
-        
-    # define a video capture object 
-    vid = cv2.VideoCapture(2) 
+for frame in filesInitial:
+    os.remove("CalibrationPics/Initial/"+frame)
+for frame in filesFinal:
+    os.remove("CalibrationPics/Final/"+frame)
+    
+# define a video capture object 
+vid = cv2.VideoCapture(2) 
 
-    boolean = True
-    count = 0
-    ret,frame = vid.read()
-    while(not ret):
-        ret,frame=vid.read()
-    print("Recording has started. Proceed to film your chessboard for 20 seconds.")
-    startTimer=time.time()
+boolean = True
+count = 0
+ret,frame = vid.read()
+while(not ret):
+    ret,frame=vid.read()
+print("Recording has started. Proceed to film your chessboard for 20 seconds.")
+startTimer=time.time()
+endTimer=time.time()
+while(endTimer-startTimer<20):
+
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read()
+
+    # Display the resulting frame 
+    cv2.imshow('frame', frame)
+    if ret: 
+        cv2.imwrite("CalibrationPics/Initial/frame%d.jpg" % count,frame)
+        count += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+    
     endTimer=time.time()
-    while(endTimer-startTimer<20):
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
 
-        # Capture the video frame 
-        # by frame 
-        ret, frame = vid.read()
-
-        # Display the resulting frame 
-        cv2.imshow('frame', frame)
-        if ret: 
-            cv2.imwrite("CalibrationPics/Initial/frame%d.jpg" % count,frame)
-            count += 1
-
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
-            break
-        
-        endTimer=time.time()
-    # After the loop release the cap object 
-    vid.release() 
-    # Destroy all the windows 
-    cv2.destroyAllWindows() 
-
-    InitialFrames = os.listdir("CalibrationPics/Initial")
-    for number in range(0,len(InitialFrames),50):
-        shutil.copy(f"CalibrationPics/Initial/frame{number}.jpg",f"CalibrationPics/Final/frame{number}.jpg")
+InitialFrames = os.listdir("CalibrationPics/Initial")
+for number in range(0,len(InitialFrames),50):
+    shutil.copy(f"CalibrationPics/Initial/frame{number}.jpg",f"CalibrationPics/Final/frame{number}.jpg")
